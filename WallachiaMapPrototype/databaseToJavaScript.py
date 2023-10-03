ef6a601250f6f3a7dde0d3d35f49c279ea4d9005
Adrian-Neata <44848057+Adrian-Neata@users.noreply.github.com>
2023-10-03 19:31:54 +0300

    Moved files to specific folders

--- WallachiaMapPrototype/databaseToJavaScript.py
+++ WallachiaMapPrototype/databaseToJavaScript.py
@@ -6,12 +6,12 @@ cur = con.cursor()
 
 res = cur.execute("SELECT MENTIONS.place_name, MENTIONS.place_latitude, MENTIONS.place_longitude, MENTIONS.place_status, RECORDS.year, MENTIONS.place_id, MENTIONS.record_id, MENTIONS.notes from MENTIONS, RECORDS where RECORDS.id == MENTIONS.record_id").fetchall()
 
-f = open("mentions.js", 'w')
+f = open("JavaScripts/mentions.js", 'w')
 f.write('var mentions = ' + json.dumps(res))
 f.close()
 
 res = cur.execute("SELECT RECORDS.id, RECORDS.year, RECORDS.description from RECORDS").fetchall()
 
-f = open("records.js", 'w')
+f = open("JavaScripts/records.js", 'w')
 f.write('var records = ' + json.dumps(res))
 f.close()
