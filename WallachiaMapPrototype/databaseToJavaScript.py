a78cb043b08e4f4a8e8cad2f55671533c889671c
Adrian-Neata <44848057+Adrian-Neata@users.noreply.github.com>
2023-12-12 17:04:10 +0200

    Updated Database + Added new SQL script
    - Added "Tabloul de regrupare..." record to Constanța, Tulcea and Ialomița counties from 1931;
    - Wrote SQL script to make it easier to add previously-mentioned record;
    - Tweaked Python script to ignore any mention with mention_status != "finished";

--- WallachiaMapPrototype/databaseToJavaScript.py
+++ WallachiaMapPrototype/databaseToJavaScript.py
@@ -4,13 +4,13 @@ import sqlite3
 con = sqlite3.connect("PlacesDataset.db")
 cur = con.cursor()
 
-res = cur.execute("SELECT MENTIONS.*, RECORDS.year, 0 from MENTIONS, RECORDS where RECORDS.id == MENTIONS.record_id AND MENTIONS.mention_status IS NOT NULL").fetchall()
+res = cur.execute("SELECT MENTIONS.*, RECORDS.year, 0 from MENTIONS, RECORDS where RECORDS.id == MENTIONS.record_id AND MENTIONS.mention_status == 'finished'").fetchall()
 
 f = open("JavaScripts/mentions_places.js", 'w')
 f.write('var mentions_places = ' + json.dumps(res))
 f.close()
 
-res = cur.execute("SELECT MENTIONS_MONASTERIES.*, RECORDS.year, 1 from MENTIONS_MONASTERIES, RECORDS where RECORDS.id == MENTIONS_MONASTERIES.record_id AND MENTIONS_MONASTERIES.mention_status IS NOT NULL").fetchall()
+res = cur.execute("SELECT MENTIONS_MONASTERIES.*, RECORDS.year, 1 from MENTIONS_MONASTERIES, RECORDS where RECORDS.id == MENTIONS_MONASTERIES.record_id AND MENTIONS_MONASTERIES.mention_status == 'finished'").fetchall()
 
 
 f = open("JavaScripts/mentions_monasteries.js", 'w')
@@ -23,7 +23,7 @@ f = open("JavaScripts/records.js", 'w')
 f.write('var records = ' + json.dumps(res))
 f.close()
 
-res = cur.execute("SELECT PLACES.* FROM PLACES JOIN MENTIONS ON PLACES.id = MENTIONS.place_id WHERE MENTIONS.mention_status IS NOT NULL").fetchall()
+res = cur.execute("SELECT PLACES.* FROM PLACES JOIN MENTIONS ON PLACES.id = MENTIONS.place_id WHERE MENTIONS.mention_status == 'finished'").fetchall()
 res_dict = {}
 for place in res:
     res_dict[int(place[0])] = place
