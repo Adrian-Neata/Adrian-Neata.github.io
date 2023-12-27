68a17d7d809d4a6eb32d6badc752f26f4cc81a45
Adrian-Neata <44848057+Adrian-Neata@users.noreply.github.com>
2023-12-27 20:30:09 +0200

    Removed duplicate "disbanded" or "unknown" mentions

--- WallachiaMapPrototype/SQL/CheckMultipleDisbandmentMentions.sql
+++ WallachiaMapPrototype/SQL/CheckMultipleDisbandmentMentions.sql
@@ -1,5 +1,5 @@
 SELECT place_id
 FROM MENTIONS
-WHERE place_status = 'disbanded'
+WHERE place_status = 'disbanded' OR place_status = 'united' OR place_status = 'unknown'
 GROUP BY place_id
 HAVING COUNT(*) > 1;
