df1cfda0e89b7a1658b55d67d2db1d619b1c4bc7
Adrian-Neata <44848057+Adrian-Neata@users.noreply.github.com>
2024-03-01 18:32:43 +0200

    Added Putna, Râmnicu Sărat, Tulcea and parts of Tecuci and Tutova counties from 1939 (Tablou-Anexă de împărțirea administrativă a județului...)
    - Finished adding data from Monitorul Oficial al României. Partea 1, 107, nr. 090, 19 aprilie 1939;

--- WallachiaMapPrototype/SQL/AddRecord25ToCounty.sql
+++ WallachiaMapPrototype/SQL/AddRecord25ToCounty.sql
@@ -1,4 +1,4 @@
 INSERT INTO mentions (place_id, place_name, place_commune, place_county, place_country, place_latitude, place_longitude, place_status, notes, reasoning, mention_status, record_id)
-SELECT m1.place_id, m1.place_name, m1.place_commune, m1.place_county, m1.place_country, m1.place_latitude, m1.place_longitude, m1.place_status, m1.notes, m1.reasoning, m1.mention_status,  25 as record_id
+SELECT m1.place_id, m1.place_name, m1.place_commune, m1.place_county, m1.place_country, m1.place_latitude, m1.place_longitude, m1.place_status, m1.notes, m1.reasoning, m1.mention_status, 113 as record_id
 FROM mentions m1
-WHERE record_id = 6 AND place_county=="Ialomița" AND NOT EXISTS (SELECT 1 FROM mentions where mentions.record_id=25 AND mentions.place_id=m1.place_id);
+WHERE record_id = 25 AND place_county=="Tecuci" AND NOT EXISTS (SELECT 1 FROM mentions where mentions.record_id=113 AND mentions.place_id=m1.place_id);
