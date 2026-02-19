45ac28a83d3859afdf0df566f96287ea7c17bccc
Adrian-Neata <44848057+Adrian-Neata@users.noreply.github.com>
2026-02-19 21:41:31 +0200

    Removed Map Files
    - if you were checking my repo you will have to wait until 2027 to see the final result :)

--- WallachiaMapPrototype/SQL/CheckMultipleDisbandmentMentions.sql
+++
@@ -1,5 +0,0 @@
-SELECT place_id
-FROM MENTIONS
-WHERE place_status = 'disbanded' OR place_status = 'united' OR place_status = 'unknown'
-GROUP BY place_id
-HAVING COUNT(*) > 1;
--- WallachiaMapPrototype/SQL/MarkEntries.sql
+++
@@ -1,2 +0,0 @@
-UPDATE mentions
-SET place_commune = '1' || place_commune where place_county='Tulcea' and record_id=8
